de2d346732e24d23cd51ea599994e920df7e071c
GitByRen <1402443141@qq.com>
2018-09-14 21:20:02 +0800

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,9 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @SpringBootApplication：说明这是一个SpringBoot应用
  * @SpringBootConfiguration：表示这是一个SpringBoot的配置类
  * @EnableAutoConfiguration：开启自动配置功能(比如我们没有配置SpringMVC，但是可以使用SpringMVC的功能)
- * *****SpringBoot启动时从类路径下的META-INF/spring.factories中获取EnableAutoConfiguration指定的值*****
  * @AutoConfigurationPackage：将主配置类(@SpringBootApplication标注的类)的所在的包及下面所有子包里面的所有组件扫描到Spring容器
  * 
+ * AutoConfigurationImportSelector：SpringBoot启动时从类路径下的META-INF/spring.factories中获取EnableAutoConfiguration指定的值
+ *
  * @ImportResource(locations = {"classpath:beans.xml"})：导入Spring的配置文件
  */
 
