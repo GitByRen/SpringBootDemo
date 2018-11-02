e30b8c6c12cb58dadc5e8f890ff9d7de5c62cea1
GitByRen <1402443141@qq.com>
2018-11-02 22:31:47 +0800

    review

--- src/main/java/com/example/bean/Person.java
+++ src/main/java/com/example/bean/Person.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * @ConfigurationProperties:告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
- * prefix="person"：配置文件中那个下面的所有属性进行一一映射，默认从全局配置文件读取
+ * prefix="person"：配置文件中哪个下面的所有属性进行一一映射，默认从全局配置文件读取
  * 
  * 这个组件必须是容器中的组件，才能提供功能
  * 
@@ -25,12 +25,14 @@ public class Person {
      * @Value("${person.last-name}"),@Value("#{22}"),需要一个一个配置，麻烦
      * @Value注解不支持JSR303数据校验,@ConfigurationProperties支持
      * @ConfigurationProperties不支持SPEL,@Value支持
+     * @ConfigurationProperties支持复杂封装，@Value不支持
      */
     private String              lastName;
     private Integer             age;
     private Boolean             boss;
     private Date                birth;
 
+//    @Value("person.maps")  启动报错，不支持复杂封装
     private Map<String, Object> maps;
     private List<Object>        lists;
     private Dog                 dog;
